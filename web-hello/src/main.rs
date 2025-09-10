7ae7eab658885abba5102a8ec9a4b29f74057292
garenaud <grenaud-@student.42lausanne.ch>
2025-09-10 16:42:54 +0200

    installation watch cargo, hotreload pour rust, amelioration docker

--- web-hello/src/main.rs
+++ web-hello/src/main.rs
@@ -7,14 +7,18 @@ use tokio::net::TcpListener;
 use std::net::SocketAddr;
 
 async fn hello() -> &'static str {
-    "Hello, Rust! 🚀"
+    "Hello, Rust! 🚀 ca fonctionne"
+}
+
+async fn health() -> &'static str {
+    "OK c'est bon"
 }
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
         .route("/", get(hello))
-        ;
+        .route("/health", get(health));
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
     println!("Listening on http://{}", addr);
