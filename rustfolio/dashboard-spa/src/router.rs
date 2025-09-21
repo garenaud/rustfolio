66fb164d602c27e0a38f1ebc76b6221cd9199f7f
garenaud <gaetan.renaud2300@gmail.com>
2025-09-21 09:20:22 +0200

    correction, reparation dependence

--- rustfolio/dashboard-spa/src/router.rs
+++ rustfolio/dashboard-spa/src/router.rs
@@ -1,7 +1,8 @@
 use yew::prelude::*;
 use yew_router::prelude::*;
 
-use crate::pages::{account, overview, profile, builder};
+use crate::pages::{account, overview, builder};
+use crate::pages::profile::ProfilePage;
 
 #[derive(Routable, PartialEq, Clone, Debug)]
 pub enum Route {
@@ -18,7 +19,7 @@ pub enum Route {
 pub fn switch(route: Route) -> Html {
     match route {
         Route::Overview => html! { <overview::Overview /> },
-        Route::Profile  => html! { <profile::Profile /> },
+        Route::Profile => html! { <ProfilePage /> },
         Route::Account  => html! { <account::Account /> },
         Route::Builder  => html! { <builder::BuilderPage /> },
     }
